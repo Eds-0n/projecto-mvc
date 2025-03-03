7540c8f29a96b8ff1c64d0410808ac2763f98f65
Eds-0n <edson.drk09@gmail.com>
2025-03-03 02:37:53 +0200

    Adicão deo Response e o Request no Projecto com sucesso

--- app/Http/Request.php
+++ app/Http/Request.php
@@ -12,19 +12,19 @@ class Request
     private $httpMethod;
 
     /**
-     * URI d página
+     * URI da página
      * @var string
      */
     private $uri;
 
     /**
-     * Parametros da URL($_GET)
+     * Parametros da URL ($_GET)
      * @var array
      */
     private $queryParams = [];
 
     /**
-     * VAriáveis recebidas no Post da Página($_POST)
+     * VAriáveis recebidas no Post da Página ($_POST)
      * @var array
      */
     private $postVars = [];
@@ -40,6 +40,50 @@ class Request
      */
     public function __construct()
     {
-        // $this
+        $this->queryParams = $_GET ?? [];
+        $this->postVars = $_POST ?? [];
+        $this->headers = getallheaders();
+        $this->httpMethod = $_SERVER['REQUEST_METHOD'] ?? '';
+        $this->uri = $_SERVER['REQUEST_URI'] ?? '';
+    }
+
+    /**
+     * Método responsável por retornar o método HTTP da requisição
+     * @return string
+     */
+    public function getHttpMethod() {
+        return $this->httpMethod;
+    }
+
+    /**
+     * Método responsável por retornar a URI da requisição
+     * @return string
+     */
+    public function getUri() {
+        return $this->uri;
+    }
+
+    /**
+     * Método responsável por retornar os headers da requisição
+     * @return array
+     */
+    public function getHeaders() {
+        return $this->headers;
+    }
+
+    /**
+     * Método responsável por retornar os parametros da URL da requisição
+     * @return array
+     */
+    public function getQueryParams() {
+        return $this->queryParams;
+    }
+
+    /**
+     * Método responsável por retornar as variáveis POST da requisição
+     * @return array
+     */
+    public function getPostVars() {
+        return $this->postVars;
     }
 }
