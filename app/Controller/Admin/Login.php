4cacc1f1ef42f273f14a3ae5ca24c0e41cadd8b5
Eds-0n <edson.drk09@gmail.com>
2025-03-21 22:15:44 +0200

    Adicionando recurso de prevenção contra acesso externo, implementação de middlewares para redirecionar o usuario a páginas especificas. Implementação de rotas para o URL's amigaveis

---
+++ app/Controller/Admin/Login.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Utils\View;
+use App\Model\Entity\User;
+use App\Session\Admin\Login as SessionAdminLogin;
+
+class Login extends Page
+{
+    /**
+     * Método responsável por retornar a renderização da página de login
+     * @param Request $request
+     * @param string $erroMessage
+     * @return string
+     */
+    public static function getLogin($request, $errorMessage = null)
+    {
+        // STATUS
+        $status = !is_null($errorMessage) ? View::render('admin/login/status', [
+            'mensagem' => $errorMessage
+        ]) : '';
+
+        // CONTEÚDO DA PÁGINA DE LOGIN
+        $content = View::render('admin/login', [
+            'status' => $status
+        ]);
+
+        // RETORNA A PÁGINA COMPLETA
+        return parent::getPage('login > Driver Labs', $content);
+    }
+
+    /**
+     * Método responsável por definir o login do usuário
+     * @param Request $request
+     * @return string
+     */
+    public static function setLogin($request)
+    {
+        // POST VARS
+        $postVars = $request->getPostVars();
+        $email = $postVars['email'] ?? '';
+        $senha = $postVars['senha'] ?? '';
+
+        // BUSCA O USUÁRIO PELO E-MAIL
+        $obUser = User::getUserByEmail($email);
+
+        // VERIFICA SE É UM USUÁRIO
+        if (!$obUser instanceof User) {
+            return self::getLogin($request, 'E-mail ou senha inválidos!');
+        }
+
+        // VERIFICA A SENHA DO USUÁRIO
+        if (!password_verify($senha, $obUser->senha)) {
+            return self::getLogin($request, 'E-mail ou senha inválidos!');
+        }
+
+        // CRIA A SESSÃO DE LOGIN
+        SessionAdminLogin::login($obUser);
+
+        // REDIRECIONA O USUÁRIO PARA A HOME DO ADMIN
+        $request->getRouter()->redirect('/admin');
+    }
+
+    /**
+     * Método responsável por deslogar o usuário
+     * @param Request $request
+     */
+    public static function setLogout($request) 
+    {
+        // DESTROI A SESSÃO DE LOGIN
+        SessionAdminLogin::logout();
+        
+        // REDIRECIONA O USUÁRIO PARA A TELA DE LOGIN
+        $request->getRouter()->redirect('/admin/login');
+    }
+}
---
+++ routes/admin.php
@@ -0,0 +1,44 @@
+<?php
+
+use \App\Http\Response;
+use \App\Controller\Admin;
+
+// ROTA ADMIN
+$obRouter->get('/admin', [
+    'middlewares' => [
+        'require-admin-login'
+    ],
+    function () {
+        return new Response(200, 'Admin :)');
+    }
+]);
+
+// ROTA LOGIN
+$obRouter->get('/admin/login', [
+    'middlewares' => [
+        'require-admin-logout'
+    ],
+    function ($request) {
+        return new Response(200, Admin\Login::getLogin($request));
+    }
+]);
+
+// ROTA LOGIN (POST)
+$obRouter->post('/admin/login', [
+    'middlewares' => [
+        'require-admin-logout'
+    ],
+    function ($request) {
+        return new Response(200, Admin\Login::setLogin($request));
+    }
+]);
+
+// ROTA LOGOUT
+$obRouter->get('/admin/logout', [
+    'middlewares' => [
+        'require-admin-login'
+    ],
+    function ($request) {
+        return new Response(200, Admin\Login::setLogout($request));
+    }
+]);
