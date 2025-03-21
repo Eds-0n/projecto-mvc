8f30af960d78fb433b0f011b9583ce09b82f7001
Eds-0n <edson.drk09@gmail.com>
2025-03-21 12:25:52 +0200

    conclusão de implementação dos middelwares

--- includes/app.php
+++ includes/app.php
@@ -3,8 +3,9 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\Utils\View;
-use App\Common\Environment;
 use App\Db\Database;
+use App\Common\Environment;
+use App\Http\Middleware\Queue as MiddlewareQueue;
 
 // CARREGA AS VARIÁVEIS DE AMBIENTE DO PROJECTO
 Environment::load(__DIR__ . '/../');
@@ -24,4 +25,14 @@ define('URL', getenv('URL'));
 // DEFINE O VALOR PADRÃO DAS VARIÁVEIS
 View::init([
     'URL' => URL
+]);
+
+// DEFINE O MAPEAMENTO DE MIDDLEWARES
+MiddlewareQueue::setMap([
+    'maintenance' => \App\Http\Middleware\Maintenance::class
+]);
+
+// DEFINE O MAPEAMENTO DE MIDDLEWARES PADRÕES
+MiddlewareQueue::setDefault([
+    'maintenance'
 ]);
